d6a2c45986f90c8bd7c5890e3fbaebebd01fa9f5
HamoriPisti <pisti.hamori@gmail.com>
2026-07-12 23:57:42 +0200

    A ScreenContainer megvalósítása, ezzel azt értem el, hogy van egy alap base Screen, amire épül a többi. Az alap beállításokat egy helyen kell majd változtatni.

--- app/src/main/java/com/example/pecatars/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pecatars/ui/screens/HomeScreen.kt
@@ -1,25 +1,19 @@
 package com.example.pecatars.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.pecatars.ui.components.MenuCard
+import com.example.pecatars.ui.components.ScreenContainer
 
 @Composable
 fun HomeScreen(
     onPackingClick: () -> Unit
 ) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(WindowInsets.safeDrawing.asPaddingValues())) {
+    ScreenContainer {
         Text(modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
--- app/src/main/java/com/example/pecatars/ui/screens/PackingScreen.kt
+++ app/src/main/java/com/example/pecatars/ui/screens/PackingScreen.kt
@@ -1,12 +1,17 @@
 package com.example.pecatars.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pecatars.model.PackingList
 import com.example.pecatars.ui.components.ListCard
+import com.example.pecatars.ui.components.ScreenContainer
 
 @Composable
 fun PackingScreen (){
@@ -15,13 +20,21 @@ fun PackingScreen (){
         PackingList(2, "Úszós peca"),
         PackingList(3, "Pergetés")
     )
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ){
-        items(lists) { packingList ->
-            ListCard(
-                packingList = packingList)
+    ScreenContainer {
+        Text(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            text = "Pakolás segéd")
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ){
+
+            items(lists) { packingList ->
+                ListCard(
+                    packingList = packingList)
+            }
         }
     }
+
 
 }
